8c7ec4cd6f7ddeb2c6264666da95cb956871e2ee
Gabriel <gabrielabiliobferreira@yahoo.com.br>
2024-03-25 22:44:37 -0300

    visualization tech

--- data_vis_und.py
+++ data_vis_und.py
@@ -1,6 +1,8 @@
 from data_loading import create_df_new
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = create_df_new()
 
@@ -34,6 +36,14 @@ def dispersion(col):
     print(f"For column {col}, we have:")
     print(f"Variance = {variance}, Std = {std}, First Quartile = {quantiles[0.25]}, Second Quartile = {quantiles[0.5]}, Third Quartile = {quantiles[0.75]}, Range = {range}")
 
+# Auxiliary definitions for the plots class:
+
+list_years = df["year"].unique()
+list_goals = []
+
+for i in list_years:
+    list_goals.append(df[df["year"] == i][["home_team_goals", "away_team_goals"]].values.sum())
+
 # Definition of the numerical columns:
 #    
 # numerical = ["home_team_goals", "away_team_goals"]
@@ -42,8 +52,8 @@ def dispersion(col):
 # Classes about data understanding:    
 
 class NonNumerical_based:
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
+    def __init__(self):
+        pass
 
     def all_teams(self):
         teams_wc_h = df["home_team_names"].unique()
@@ -77,25 +87,101 @@ class NonNumerical_based:
         return goal_per_team
     
 class Numerical_based:
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
+    def __init__(self, numerical):
+        self.dataframe_numerical = numerical
 
-    def central_tedency_all(self, numerical):
-        for i in numerical:
+    def central_tedency_all(self):
+        for i in self.dataframe_numerical :
             central_tedency(i)
     
-    def dispersion_all(self, numerical):
-        for i in numerical:
+    def dispersion_all(self):
+        for i in self.dataframe_numerical :
             dispersion(i)
 
     def goals_per_game_stat(self):
-        goals_per_game = list(df[["home_team_goals", "away_team_goals"]].sum(axis = 1))
+        goals_per_game = list(df[self.dataframe_numerical].sum(axis = 1))
         goals_per_game = np.array(goals_per_game)
 
         print(f"Total number of goals in all matches at WC history: {goals_per_game.sum()}\nMean of goals per game: {goals_per_game.mean()}\nStandard Deviation of goals per game: {goals_per_game.std()}\nVariance of goals per game: {goals_per_game.var()}\nMaximum number of goals per game: {goals_per_game.max()}")
 
+# Class about data vis
+
+class Plots(NonNumerical_based):
+    def __init__(self):
+        pass
+
+    def plot_victories(self):
+        vic_team = self.victories_per_team()
+        series_aux_vic = pd.Series(range(1, 66), list(vic_team.index))
+
+        list_aux_vic = list(zip(range(1, 66), list(vic_team.index)))
+
+        print("According to the following relationship between the number and the teams, we have:")
+        print(list_aux_vic)
+        plt.figure(figsize = (20,20))
+        plt.plot(series_aux_vic.values, vic_team.fillna(0).values, "*--b",  ms = 8)
+        plt.xticks(np.arange(1, 66, 1))
+        plt.xlabel("Indexes of the teams")
+        plt.ylabel("Number of victories")
+        plt.title("Victories")
+        plt.plot()
+
+    def plot_loss(self):
+        loss_team = self.losses_per_team()
+        series_aux_loss = pd.Series(range(1, 87), list(loss_team.index))
+
+        list_aux_loss = list(zip(range(1, 87), list(loss_team.index)))
+
+        print("According to the following relationship between the number and the teams, we have:")
+        print(list_aux_loss)
+        plt.figure(figsize = (25,25))
+        plt.plot(series_aux_loss.values, loss_team.fillna(0).values, "*--r",  ms = 8)
+        plt.xticks(np.arange(1, 87, 1))
+        plt.xlabel("Indexes of the teams")
+        plt.ylabel("Number of losses")
+        plt.title("Losses")
+        plt.plot()
+
+    def games_plot(self):
+        teams = self.games_played()
+        series_aux_teams = pd.Series(range(1, 87), teams.index)
+
+        list_aux_teams = list(zip(range(1, 87), teams.index))
+
+        print("According to the following relationship between the number and the teams, we have:")
+        print(list_aux_teams)
+        plt.figure(figsize = (25,25))
+        plt.plot(series_aux_teams.values, teams.fillna(0).values, "*--g",  ms = 8)
+        plt.xticks(np.arange(1, 87, 1))
+        plt.xlabel("Indexes of the teams")
+        plt.ylabel("Number of games")
+        plt.title("Games played by each team")        
+        plt.plot()
+
+    def goals_plot(self):
+        plt.figure(figsize = (20,20))
+        plt.plot(list_years, list_goals, "^",  ms = 12)
+        plt.xticks(list_years)
+        plt.xlabel("Editions of WC")
+        plt.ylabel("Goals")
+        plt.title("Number of goals in each edition")
+        plt.plot()
+
+    def goals_reg_plot(self):
+        sns.jointplot(x = list_years, y = list_goals, kind = "reg")
+    
+    def number_of_trophies_plot(self):
+        winners = ["Spain", "England", "Uruguay", "France", "Argentina", "Italy", "Germany", "Brazil"]
+        titles = [1, 1, 2, 2, 2, 4, 4, 5]
+
+        plt.plot(winners, titles, "*", ms = 10)
+        plt.title("Titles: winners of WC")
+        plt.xlabel("Winners")
+        plt.ylabel("Titles")
+        plt.plot()
+
 if __name__ == "__main__":
     print("A library to realize data understanding about WC matches.\n")
 
-
+ 
 
